0b590f8196825d0b9a9afb68eb90554c92a72c30
DekelZoaretz <Dekelzoaretz@gmail.com>
2015-05-09 12:44:24 +0300

    First commit

--- app/src/main/java/com/example/dekel/goodworld/MainActivity.java
+++ app/src/main/java/com/example/dekel/goodworld/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String a = "Ofir";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
