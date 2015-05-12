66bc4ba38a833bf6598a575e6ec4438692383f82
DekelZoaretz <Dekelzoaretz@gmail.com>
2015-05-12 22:25:37 +0300

    Create database information

---
+++ app/src/main/java/com/example/dekel/goodworld/DatabaseInfo.java
@@ -0,0 +1,15 @@
+package com.example.dekel.goodworld;
+
+/**
+ * Created by Dekel on 5/12/2015.
+ */
+public class DatabaseInfo {
+
+    public static final String USER_NAME = "user_name";
+    public static final String USER_NUM = "user_number";
+    public static final String DATABASE_NAME = "good_world";
+    public static final String TABLE_NAME = "massages";
+
+
+
+}
